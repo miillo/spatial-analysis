0faa615313c30c91dcac7360d63a98dd541d7ccc
Miłosz Pluta <milosz.pluta2@gmail.com>
2019-05-04 16:10:54 +0200

    Normalizing latitude/longitude + drawing world map

--- spatial-analysis.r
+++ spatial-analysis.r
@@ -1,7 +1,21 @@
-##### Data preprocessing #####
-# dla porzadku mozna nazwac kolumny
+library(rworldmap)
 
-sourceData = read.fwf("CLIWOC21CORE.txt", widths = c(4,2,2,4,5,6,2,1,1,1,1,1,2,2,9,2,1,3,1,3,1,2,2,1,5,1,3,1,4,1,4,1,4,2,4,1,1,1,1,1,1,1,2,2,2,2,2,2))
-                                          select(d,1,2,3,4,5,6,14,15,16,18,19,20,25,28,29,35)
+##### Data preprocessing #####
+# Reading source data 
+sourceData = read.fwf("CLIWOC21CORE.txt", widths = c(4,2,2,4,5,6,2,1,1,1,1,1,2,2,9,2,1,3,1,3,1,2,2,1,5,1,3,1,4,1,4,1,4,2,4,1,1,1,1,1,1,1,2,2,2,2,2,2), 
+                                          col.names =  c("YR","MO","DY","HR","LAT","LON","IM","ATTC","TI","LI","DS","VS","NID","II","ID","C1","DI","D","WI","W","VI","VV","WW","W1","SLP","A","PPP","IT","AT","WBTI","WBT","DPTI","DPT","SI","SST","N","NH","CL","HI","H","CM","CH","WD","WP","WH","SD","SP","SH"))
+
+# Normalizing longitude / latitude
+source("coord-normalization.r")
+
+sourceData <- transform(sourceData, LAT = latNorm(sourceData$LAT))
+sourceData <- transform(sourceData, LON = longNorm(sourceData$LON))
 
 #####  #####
+map <- getMap(resolution = 'low')
+# tutaj sa zakresy wspolrzednych mapy swiata rysowanej przez rworldmap
+map@bbox
+plot(map)                                          
+
+# naniesienie punktow na mape swiata
+points(sourceData$LON, sourceData$LAT, col = "red", cex = .6)
