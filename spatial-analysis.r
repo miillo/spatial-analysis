157f8d74f38be1e7deda4b5d3ca5a8852344dc34
Miłosz Pluta <milosz.pluta2@gmail.com>
2019-05-06 19:03:26 +0200

    Coord normalization fix + drawing route map based on coordinates

--- spatial-analysis.r
+++ spatial-analysis.r
@@ -2,20 +2,19 @@ library(rworldmap)
 
 ##### Data preprocessing #####
 # Reading source data 
+
+## core
 sourceData = read.fwf("CLIWOC21CORE.txt", widths = c(4,2,2,4,5,6,2,1,1,1,1,1,2,2,9,2,1,3,1,3,1,2,2,1,5,1,3,1,4,1,4,1,4,2,4,1,1,1,1,1,1,1,2,2,2,2,2,2), 
                                           col.names =  c("YR","MO","DY","HR","LAT","LON","IM","ATTC","TI","LI","DS","VS","NID","II","ID","C1","DI","D","WI","W","VI","VV","WW","W1","SLP","A","PPP","IT","AT","WBTI","WBT","DPTI","DPT","SI","SST","N","NH","CL","HI","H","CM","CH","WD","WP","WH","SD","SP","SH"))
 
 # Normalizing longitude / latitude
 source("coord-normalization.r")
 
-sourceData <- transform(sourceData, LAT = latNorm(sourceData$LAT))
-sourceData <- transform(sourceData, LON = longNorm(sourceData$LON))
+sourceDataNorm <- transform(sourceData, LAT = latNorm(sourceData$LAT), LON = map(sourceData$LON, longNorm))
 
 #####  #####
 map <- getMap(resolution = 'low')
-# tutaj sa zakresy wspolrzednych mapy swiata rysowanej przez rworldmap
-map@bbox
 plot(map)                                          
 
 # naniesienie punktow na mape swiata
-points(sourceData$LON, sourceData$LAT, col = "red", cex = .6)
+points(sourceDataNorm$LON, sourceDataNorm$LAT, col = "red", cex = .1)
